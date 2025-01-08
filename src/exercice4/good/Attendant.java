e7b763259100a5569d2998f1817d457241d80abd
joaocandidozup <joao.candido@zup.com>
2025-01-08 09:15:13 -0300

    feat: implement ClientAttendant interface in Attendant class

---
+++ src/exercice4/good/Attendant.java
@@ -0,0 +1,11 @@
+package exercice4.good;
+
+import exercice4.bad.Employee;
+
+public class Attendant implements ClientAttendant {
+
+
+    public void serveCustomer() {
+        System.out.println("Atendendo cliente...");
+    }
+}

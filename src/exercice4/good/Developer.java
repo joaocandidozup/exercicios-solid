d18022a62f17763a6a2923acd0b18b401de182c3
joaocandidozup <joao.candido@zup.com>
2025-01-08 09:10:43 -0300

    feat: implement Programmer interface in Developer class

---
+++ src/exercice4/good/Developer.java
@@ -0,0 +1,11 @@
+package exercice4.good;
+
+import exercice4.bad.Employee;
+
+public class Developer implements Programmer {
+    public void program() {
+        System.out.println("Programando...");
+    }
+
+
+}
